3eba0a10fdd412598b6154e43fac9c600f409db6
Nazar Kovalchuk <nazar.kovalchuk17@gmail.com>
2020-04-24 18:07:10 +0300

    v 1.3

--- source/js/main.js
+++ source/js/main.js
@@ -1,38 +1,72 @@
 $("select").styler();
 
-let form = document.getElementById("form"),
-  sub = document.getElementById("sub"),
-  name = document.getElementById("name"),
-  phone = document.getElementById("phone"),
-  email = document.getElementById("email"),
-  area = document.getElementById("area");
+function validateName() {
+  var n = document.forms["formbox"]["name"].value;
 
-console.log(name);
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  checkInputs();
-});
-
-function checkInputs() {
-  const subValue = sub.value.trim();
-  const nameValue = name.value.trim();
-  const phoneValue = phone.value.trim();
-  const emailValue = email.value.trim();
-  const areaValue = area.value.trim();
-
-  if (nameValue == "") {
-    setErrorFor(name, "Username cannot be blank");
+  if (n.length < 2 || n.length > 20 || !isNaN(n)) {
+    document.getElementById("name").classList.add("error");
+    document.getElementById("span__name").classList.remove("hiden");
+    return false;
   } else {
-    setSuccesFor(name);
+    document.getElementById("name").classList.remove("error");
+    document.getElementById("span__name").classList.add("hiden");
+    return true;
+  }
+}
+function validatePhone() {
+  var phone = document.forms["formbox"]["phone"].value;
+
+  if (phone.length < 10) {
+    document.getElementById("phone").classList.add("error");
+    document.getElementById("span__phone").classList.remove("hiden");
+    return false;
+  } else {
+    document.getElementById("phone").classList.remove("error");
+    document.getElementById("span__phone").classList.add("hiden");
+    return true;
+  }
+}
+function validateEmail() {
+  var x = document.forms["formbox"]["email"].value;
+
+  var atpos = x.indexOf("@");
+  var dotpos = x.lastIndexOf(".");
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+    document.getElementById("email").classList.add("error");
+    document.getElementById("span__mail").classList.remove("hiden");
+    return false;
+  } else {
+    document.getElementById("email").classList.remove("error");
+    document.getElementById("span__mail").classList.add("hiden");
+    return true;
+  }
+}
+function validateMessage() {
+  var message = document.forms["formbox"]["area"].value;
+
+  if (message.length < 5) {
+    document.getElementById("area").classList.add("error");
+    document.getElementById("span__message").classList.remove("hiden");
+    return false;
+  } else {
+    document.getElementById("area").classList.remove("error");
+    document.getElementById("span__message").classList.add("hiden");
+    return true;
+  }
+}
+document.getElementById("send__btn").onclick = function () {
+  document.forms.my.reset();
+  location.reload();
+};
+function checkall() {
+  if ((validateEmail(), validateName(), validateMessage(), validatePhone())) {
+    document.getElementById("send__message").classList.remove("hiden");
+    return false;
+  } else {
+    return false;
   }
 }
 
-function setErrorFor(input, message) {
-  const formcontrol = inpur.parentElement;
-  const small = formcontrol.querySelector("span");
-
-  small.innerText = message;
-
-  formcontrol.classname = "form__label error";
-}
+$(".jq-selectbox__select").on("click", function () {
+  $(".line").slideToggle();
+});
